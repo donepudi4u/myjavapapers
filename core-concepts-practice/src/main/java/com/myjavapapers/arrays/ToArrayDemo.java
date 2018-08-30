e83bb4df053d8ae47a184a3ee71a65a9ab637133
Dileep Donepudi <donepudi4u@gmail.com>
2018-08-30 06:34:58 +0530

    removed worng package name

---
+++ core-concepts-practice/src/main/java/com/myjavapapers/arrays/ToArrayDemo.java
@@ -0,0 +1,15 @@
+package com.myjavapapers.arrays;
+
+import com.myjavapapers.beans.EmployeeBean;
+import com.myjavapapers.spring.model.Employee;
+
+public class ToArrayDemo {
+
+    public static void main(String[] args) {
+        List<EmployeeBean> employees = new ArrayList<EmployeeBean>();
+        employees.add(new EmployeeBean());
+
+
+    }
+
+}
